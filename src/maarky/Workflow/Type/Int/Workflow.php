a4fb987e06ebc30b8d62e21002d0e3ff0e300c5a
Mark Wright <sparkymeister@gmail.com>
2018-03-23 17:52:40 -0700

    new Numeric type, extended by int and float

--- src/maarky/Workflow/Type/Int/Workflow.php
+++ src/maarky/Workflow/Type/Int/Workflow.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace maarky\Workflow\Type\Int;
 
-abstract class Workflow extends \maarky\Workflow\Workflow
+abstract class Workflow extends \maarky\Workflow\Type\Numeric\Workflow
 {
     public static function isValid($value): bool
     {
---
+++ src/maarky/Workflow/Type/Numeric/Success.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace maarky\Workflow\Type\Numeric;
+
+use maarky\Workflow\Component\BaseSuccess;
+
+class Success extends Workflow
+{
+    use BaseSuccess;
+}
